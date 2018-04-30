3ef603b5eec3f9ee7d7ce64e937ff97baaf96ae5
unmesh <unmeshjoshi@gmail.com>
2018-04-30 22:03:02 +0530

    changing no. of threads to 10000

--- server/src/main/java/org/thoughtworks/com/JettyServer.java
+++ server/src/main/java/org/thoughtworks/com/JettyServer.java
@@ -13,7 +13,10 @@ public class JettyServer
     public static void main( String[] args ) throws Exception {
         Server server = new Server(8080);
 
-        server.setThreadPool(new IO());
+        IO threadPool = new IO();
+        threadPool.setMaxThreads(10000);
+        server.setThreadPool(threadPool);
+
         System.out.println(server.getThreadPool());
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(PingHandler.class, "/");
